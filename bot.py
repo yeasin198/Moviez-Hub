fee124e25c8f9a981a49c910c114f8f69c6c2040
yeasin198 <ya2620831@gmail.com>
2025-07-14 20:52:35 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -1074,7 +1074,7 @@ def telegram_webhook():
                     print(f"Error processing /start command: {e}")
                     requests.get(f"{TELEGRAM_API_URL}/sendMessage", params={'chat_id': chat_id, 'text': "An unexpected error occurred while processing your request."})
             else:
-                requests.get(f"{TELEGRAM_API_URL}/sendMessage", params={'chat_id': chat_id, 'text': "আমাদের movieflix9u.onrender.com ওয়েবসাইটে আপনাকে স্বাগতম.                                                                                                                                                                          আমাদের অপিসিয়াল চ্যানেলে জয়েন করুন @MovieFlix9u  আমাদের অপিসিয়াল গ্রুপে জয়েন করুন @MovieFlix9uChat  আমাদের অপিসিয়াল চ্যানেলে ও গ্রুপে জয়েন করার জন্য আপনাকে ধন্যবাদ."})
+                requests.get(f"{TELEGRAM_API_URL}/sendMessage", params={'chat_id': chat_id, 'text': "আমাদের moviezhub.onrender.com ওয়েবসাইটে আপনাকে স্বাগতম.                                                                                                                                                                          আমাদের অপিসিয়াল চ্যানেলে জয়েন করুন @Moviez_Hub_Official  আমাদের অপিসিয়াল গ্রুপে জয়েন করুন @moviez_hub_discussion  আমাদের অপিসিয়াল চ্যানেলে ও গ্রুপে জয়েন করার জন্য আপনাকে ধন্যবাদ.                                                                                                                                                                          Develooper By:@YABOTZ ."})
                 
     return jsonify(status='ok')
 
